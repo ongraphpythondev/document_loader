d49b0538a42a5030447cb50a9de54ba933d14ca5
ongraphpythondev <pythondev@ongraph.com>
2024-07-29 14:07:26 +0530

    update code to get api keys from the sidebar and add docstring

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
@@ -12,101 +11,105 @@ from langchain.embeddings import HuggingFaceEmbeddings
 import tiktoken
 from langchain.callbacks import get_openai_callback
 import os
+from openai import AuthenticationError 
 
-# Sidebar contents
-    
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
-load_dotenv()
-try: 
-    openai_api_key=st.secrets["OPENAI_API_KEY"]
-    max_token_user=st.secrets["MAX_TOKEN_USER"]
-    demo=st.secrets["DEMO"]#set demo 1
-except:
-    max_token_user=os.getenv("MAX_TOKEN_USER")
-    openai_api_key=os.getenv('OPENAI_API_KEY')
-    demo=os.getenv('DEMO')
-def main():
-    
-    if demo:
-        with st.sidebar:
-            st.title('🤗💬 LLM Chat App')
-            st.markdown('''
-            ## About
-            This app is an LLM-powered chatbot built using:
-            - [Streamlit](https://streamlit.io/)
-            - [LangChain](https://python.langchain.com/)
-            - [OpenAI](https://platform.openai.com/docs/models) LLM model
-
-            ''')
-            add_vertical_space(5)
-        st.header("Chat with PDF 💬")
+st.header("Chat with PDF 💬")
+subheader = st.subheader('Enter an API key in the sidebar to analyze your report.',divider=True)
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - https://streamlit.io/
+    - https://python.langchain.com/
+    - https://platform.openai.com/docs/models LLM model
+    ''')
+    openai_api_key = st.text_input("Enter OpenAI API Key", type="password")
+    add_vertical_space(5)
+        
 
 
-        # upload a PDF file
-        pdf = st.file_uploader("Upload your PDF", type='pdf')
+def extract_text_from_pdf(pdf):
+    """
+    Extracts text from a PDF file.
 
-        # st.write(pdf)
-        if pdf is not None:
-            pdf_reader = PdfReader(pdf)
-            
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
+    Args:
+        pdf: The PDF file to extract text from (can be a file path or file-like object).
 
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=1000,
-                chunk_overlap=200,
-                length_function=len
-                )
-            chunks = text_splitter.split_text(text=text)
+    Returns:
+        str: Extracted text from the PDF.
+    """
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-            # # embeddings
-            store_name = pdf.name[:-4]
-            st.write(f'{store_name}')
-            # st.write(chunks)
 
-            if os.path.exists(f"{store_name}.pkl"):
-                with open(f"{store_name}.pkl", "rb") as f:
-                    VectorStore = pickle.load(f)
-                # st.write('Embeddings Loaded from the Disk')s
-            else:
-                # embeddings = OpenAIEmbeddings()
+def process_text_and_load_vector_store(text):
+    """
+    Splits the given text into chunks, loads or creates a vector store, and returns it.
+
+    Args:
+        text (str): The text to be split and processed.
+
+    Returns:
+        VectorStore: The vector store loaded from a file or created from the text chunks.
+    """
                 
-                embeddings=HuggingFaceEmbeddings()
-                VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-                with open(f"{store_name}.pkl", "wb") as f:
-                    pickle.dump(VectorStore, f)
-
-            # embeddings = OpenAIEmbeddings()
-            # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-
-            # Accept user questions/query
-            query = st.text_input("Ask questions about your PDF file:")
-            # st.write(query)
-            user_token=num_tokens_from_string(query, "gpt-3.5-turbo")
-            print("USER TOKEN COUNT: ", user_token)
-            if query:
-                if user_token < int(max_token_user):
-                    docs = VectorStore.similarity_search(query=query, k=3)
-
-                    llm = OpenAI(openai_api_key=openai_api_key,temperature=0,)
-                    chain = load_qa_chain(llm=llm, chain_type="stuff")
-                    
-
-                    with get_openai_callback() as cb:
-                        response = chain.run(input_documents=docs, question=query)
-                        print(cb)
-                    st.write(response)
-                else:
-                    st.write(f"EXCEED ALLOCATED PROMPT,\n MAX TOKEN: {max_token_user} \n YOUR TOKEN: {user_token}")
-
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+        )
+    chunks = text_splitter.split_text(text=text)
+    store_name = pdf.name[:-4]
+    st.write(f'{store_name}')
+    if os.path.exists(f"{store_name}.pkl"):
+        with open(f"{store_name}.pkl", "rb") as f:
+            VectorStore = pickle.load(f)
     else:
-        st.header("This App is Private!!!")
+        
+        embeddings=HuggingFaceEmbeddings()
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(VectorStore, f)
+    return VectorStore
+
+def main():
+    """
+    Main function to handle PDF text extraction, vector store processing, and user query handling.
+    
+    - Extracts text from a PDF file.
+    - Processes the text and loads or creates a vector store.
+    - Accepts user input for queries about the PDF.
+    - Checks token count and performs a similarity search if within token limits.
+    - Uses an LLM to generate a response based on the similarity search results.
+    """
+
+    text = extract_text_from_pdf(pdf)
+    VectorStore = process_text_and_load_vector_store(text)
+    query = st.text_input("Ask questions about your PDF file:")
+    try:
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+            llm = OpenAI(openai_api_key=openai_api_key,temperature=0,)
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+    except AuthenticationError:
+        st.warning(
+            body='AuthenticationError : Please provide correct api key 🔑' ,icon='🤖')
+    except Exception as e:
+        st.warning(f"An error occure while processing the query: {e}")
+
 
 if __name__ == '__main__':
-    main()
+    if openai_api_key:
+        subheader.empty()
+        pdf = st.file_uploader("Upload your PDF", type='pdf')
+        if pdf:
+            main()
